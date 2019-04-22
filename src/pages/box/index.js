c3e3e90a7f5fa0371a16ccc7f4a7b364214311bf
Anderson <philipe0611@gmail.com>
2019-04-22 08:57:39 -0300

    inclusao da page boxes

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,12 +1,52 @@
 import React, { Component } from  'react';
+//import logo from '../../assets/parachute-box.png'
+import logo from '../../assets/box.svg'
 import './style.css';
+import api from '../../services/api';
 // import logo from '../../assets/parachute-box.png';
 // import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        box: {},
+    }
+    
+    async componentDidMount(){
+        const caixa = this.props.match.params.id;
+        const response = await api.get(`/boxes/${caixa}`);
+        
+        this.setState({box: response.data});
+    }
+
     render() {
         return (
-            <h1> </h1>
+            <div className="boxContainer"> 
+                <header className="header">
+                    <img src={logo} alt="logo" className="logoHeader"/>
+                    <h2>BOX: {this.state.box.title}</h2>
+                </header>
+
+                <ul className="list">
+                <span>Arquivos:</span>
+                {this.state.box.files && (
+                    <li className="itemList">
+                        <p>Não existem arquivos gravados na Box: <strong>{this.state.box.title}</strong></p>
+                    </li>
+                )}
+
+                {this.state.box.files && this.state.box.files.map(files => (
+                    <li className="itemList">
+                        <div>
+                            <a href={files.url}>
+                            <i class="fas fa-file"></i>
+                            <strong>{files.title}</strong>                           
+                            </a> 
+                            <span>date</span>
+                        </div>
+                    </li>
+                )) }                              
+                </ul>
+            </div>
         );
     }
 }
